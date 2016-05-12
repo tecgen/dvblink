8b09a39ece76bf1156c148f6e145dde6d47d89bc
tecgen <no@email.com>
2016-05-12 21:31:25 +0200

    refactored to a function

--- correctName.py
+++ correctName.py
@@ -1,4 +1,4 @@
-# This script returns a human readable name for files record by DVBLink server 
+# This function returns a human readable name for files record by DVBLink server 
 #
 # EXAMPLE:
 # python correctName.py Zwei_Missionare-2014_20160512-302-1036000-1463076848.ts
@@ -9,20 +9,21 @@
 import sys
 import string
 
-# get first argument
-input = sys.argv[1]
+''' returns a human readable name for files record by DVBLink server '''
+def correctName(input) :
+	# replace all underlines with spaces
+	output = string.replace(input, '_', ' ')
 
-# replace all underlines with spaces
-output = string.replace(input, '_', ' ')
+	# split into list elements
+	output = string.split(output, '-')
 
-# split into list elements
-output = string.split(output, '-')
+	# file name is the first element
+	filename = output[0]
 
-# file name is the first element
-filename = output[0]
+	extension = string.split(input, '.')
+	extension = extension[1]
+	return (filename + '.' + extension)	
 
-extension = string.split(input, '.')
-extension = extension[1]
 
-print(filename + '.' + extension)
+print(correctName(sys.argv[1]))
 
